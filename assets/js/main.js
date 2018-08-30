7a5d8530787260c1bf4b5d8d96cd1c3cafdb0bd7
Bennett Cheung <bennettcheung@yahoo.com>
2018-08-29 17:39:47 -0700

    added more javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,51 @@
 jQuery(document).ready(function($) {
 
     $('.level-bar-inner').css('width', '0');
-    
+
     $(window).on('load', function() {
 
         $('.level-bar-inner').each(function() {
-        
+
             var itemWidth = $(this).data('level');
-            
+
             $(this).animate({
                 width: itemWidth
             }, 800);
-            
+
         });
 
     });
-   
-    
+
+
 
 });
+
+    // -------------------------------------------------------------
+    // Shuffle
+    // -------------------------------------------------------------
+
+    (function () {
+
+        var $grid = $('#grid');
+
+        $grid.shuffle({
+            itemSelector: '.item'
+        });
+
+        /* reshuffle when user clicks a filter item */
+        $('#filter a').click(function (e) {
+            e.preventDefault();
+
+            // set active class
+            $('#filter a').removeClass('active');
+            $(this).addClass('active');
+
+            // get group name from clicked item
+            var groupName = $(this).attr('data-group');
+
+            // reshuffle grid
+            $grid.shuffle('shuffle', groupName );
+        });
+
+
+    }());
