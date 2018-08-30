295f699d553bcfe045c1e557bf30f886a2b25b9f
Bennett Cheung <bennettcheung@yahoo.com>
2018-08-29 23:38:29 -0700

    Added jquery logic in experience section

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,36 +16,11 @@ jQuery(document).ready(function($) {
 
     });
 
-
+    $("#allExp").click(function(){
+            $(".support").slideDown();
+        });
+    $("#softwareExp").click(function(){
+        $(".support").slideUp();
+    });
 
 });
-
-    // -------------------------------------------------------------
-    // Shuffle
-    // -------------------------------------------------------------
-
-    (function () {
-
-        var $grid = $('#grid');
-
-        $grid.shuffle({
-            itemSelector: '.item'
-        });
-
-        /* reshuffle when user clicks a filter item */
-        $('#filter a').click(function (e) {
-            e.preventDefault();
-
-            // set active class
-            $('#filter a').removeClass('active');
-            $(this).addClass('active');
-
-            // get group name from clicked item
-            var groupName = $(this).attr('data-group');
-
-            // reshuffle grid
-            $grid.shuffle('shuffle', groupName );
-        });
-
-
-    }());
